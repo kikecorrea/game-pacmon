69e83048e9829b4263ca122a59168fb83f35c89e
mailboy1412 <mailboy1412@gmail.com>
2012-03-05 09:14:00 +0000

    first working ball movement and Thread to handle animation

--- src/com/csc780/pacmon/GameActivity.java
+++ src/com/csc780/pacmon/GameActivity.java
@@ -1,13 +1,75 @@
 package com.csc780.pacmon;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
-public class GameActivity extends Activity {
+public class GameActivity extends Activity implements SensorEventListener{
+	
+	
+	private GameSurfaceView gameView;
+	private SensorManager mySensorManager;
+	private Sensor myAccelerometer;
+	
+	//change in x and y of pac-mon
+	private float xAccel;
+	private float yAccel;
+	private float accelerometerNoise = 1.F; 
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game);
+        
+        mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        myAccelerometer = mySensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mySensorManager.registerListener(this, myAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        gameView = new GameSurfaceView(this);
+        
+        setContentView(gameView);
+        
     }
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		gameView.pause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		gameView.resume();
+	}
+
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//get values of accelerometer
+	public void onSensorChanged(SensorEvent event) {
+		
+		xAccel = event.values[0];
+		yAccel = event.values[1];
+		float z = event.values[2];
+		
+		
+		
+		gameView.setDx(-xAccel);
+		gameView.setDy(yAccel);
+		
+		
+	}
+    
+    
+    
+    
 }
--- src/com/csc780/pacmon/GameSurfaceView.java
+++ src/com/csc780/pacmon/GameSurfaceView.java
@@ -1,44 +1,110 @@
 package com.csc780.pacmon;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameSurfaceView extends SurfaceView implements Runnable {
 
-	Thread surfaceThread = null;
-	boolean isRunning;
+	private float screenWidth, screenHeight;
+	private float dx, dy;
+	private float ballTopX, ballTopY;
+	private SurfaceHolder surfaceHolder;
+	private Thread surfaceThread = null;
+	boolean isRunning = false;
+	private Bitmap ball;
 	
 	public GameSurfaceView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 		
-		isRunning = true;
-		surfaceThread = new Thread(this);
-		surfaceThread.start();
-	}
+		//screenWidth = 480;//this.getWidth();
+		//screenHeight = 800;//this.getHeight();
 
+		
+		dx = dy = 0;
+		
+		surfaceHolder = getHolder();
+		ballTopX = screenWidth/2;
+		ballTopY = screenHeight/2;
+		ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+		isRunning = true;
+		setKeepScreenOn(true);
+		
+
+	}
+	
+	//thread to draw 
 	public void run() {
 		
 		while(isRunning){
+			if(!surfaceHolder.getSurface().isValid())
+				continue;
 			
+			Canvas canvas = surfaceHolder.lockCanvas();
+
+			screenWidth = canvas.getWidth();
+			screenHeight = canvas.getHeight();
+			
+			if (ballTopX + dx < 0)
+				ballTopX = 0;
+			else if (ballTopX + ball.getWidth() + dx > screenWidth)
+				ballTopX = screenWidth - ball.getWidth();
+			else 
+				ballTopX += dx;
+			
+			if (ballTopY + dy < 0)
+				ballTopY = 0;
+			else if (ballTopY + ball.getHeight() + dy > screenHeight)
+				ballTopY = screenHeight - ball.getHeight();
+			else
+				ballTopY += dy;
+			
+			canvas.drawRGB(02, 02, 150);
+			
+			canvas.drawBitmap(ball, ballTopX, ballTopY, null);
+			
+			surfaceHolder.unlockCanvasAndPost(canvas);
 		}
 	}
 
 	public void pause() {
-
+		isRunning = false;
+		while(true){
+			try {
+				surfaceThread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		}
+		surfaceThread = null;
 	}
 	
 	public void resume() {
 		isRunning = true;
+		surfaceThread = new Thread(this);
+		surfaceThread.start();
+		setKeepScreenOn(true);
 	}
+	
 	
 	@Override
 	public void draw(Canvas canvas) {
 		// TODO Auto-generated method stub
-		
-		
-		
+
+	}
+
+	public void setDx(float dx) {
+		this.dx = dx;
+	}
+
+	public void setDy(float dy) {
+		this.dy = dy;
 	}
 	
 
