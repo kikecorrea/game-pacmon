eb3f73fdddd3511a7be993b6a80e98e583f509a8
mailboy1412@gmail.com <mailboy1412@gmail.com@cffac096-095a-f1c3-f305-07a49a4519d8>
2012-03-05 09:14:00 +0000

    first working ball movement and Thread to handle animation

--- trunk/src/com/csc780/pacmon/MenuActivity.java
+++ trunk/src/com/csc780/pacmon/MenuActivity.java
@@ -1,6 +1,7 @@
 package com.csc780.pacmon;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,12 +30,37 @@ public class MenuActivity extends Activity implements OnClickListener{
 		aboutButton = (Button) findViewById(R.id.bAbout);
 		exitButton = (Button) findViewById(R.id.bExit);
 		
+		singlePlayButton.setOnClickListener(this);
+		multiPlayButton.setOnClickListener(this);
+		optionsButton.setOnClickListener(this);
+		aboutButton.setOnClickListener(this);
+		exitButton.setOnClickListener(this);
+		
 	}
 
 
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		switch (v.getId()){
 		
+		case R.id.bSingle: //start single player game
+			Intent sGame = new Intent("com.csc780.pacmon.GAMEACTIVITY");
+			startActivity(sGame);
+			break;
+			
+		case R.id.bMulti:
+			break;
+			
+		case R.id.bOptions:
+			break;
+		case R.id.bAbout:
+			break;
+			
+		case R.id.bExit: //finish the activity
+			this.finish();
+			break;
+		
+		}
 	}
 
 	
